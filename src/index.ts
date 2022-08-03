aec9ed6e57611263383864434917b2f18ab8b0e7
beingbiplov <sharmabiplov@gmail.com>
2022-08-03 18:17:02 +0545

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express, { Request, Response, Application } from 'express';
-
-import appRouter from './routes';
-import { notFound } from './middlewares/notFound';
-import { errorHandler } from './middlewares/errorHandler';
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Request, Response, Application } from "express";
 
 dotenv.config();
 
@@ -14,15 +10,12 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('API is running....');
+app.get("/", (req: Request, res: Response) => {
+  res.send("API is running....");
 });
-
-app.use(appRouter);
-
-app.use(notFound);
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
